49d5bd2b16fc0b24c1352ce828a52fd7287c8015
floriandreau <36734324+floriandreau@users.noreply.github.com>
2019-10-29 08:52:17 +0100

    Final

--- js/slide.js
+++ js/slide.js
@@ -2,6 +2,7 @@ let slide= document.getElementById('slide');
 
 var i = 0; 
 let img = document.getElementById('slide').children;
+let img_slide = document.getElementById('slide_matiere').children;
 
 function slidejs()
 {
@@ -11,19 +12,28 @@ function slidejs()
                 img[0].classList.remove('hide');
                 img[1].classList.add('hide');
                 img[2].classList.add('hide');  
-                
+
+                img_slide[0].classList.remove('hide');
+                img_slide[1].classList.add('hide');
+                img_slide[2].classList.add('hide');  
             }
             if (i == 1) {
                 img[1].classList.remove('hide');
                 img[2].classList.add('hide');
                 img[0].classList.add('hide');  
 
+                img_slide[1].classList.remove('hide');
+                img_slide[2].classList.add('hide');
+                img_slide[0].classList.add('hide');  
             }
             if (i == 2) {
                 img[2].classList.remove('hide');
                 img[1].classList.add('hide');
                 img[0].classList.add('hide');  
 
+                img_slide[2].classList.remove('hide');
+                img_slide[1].classList.add('hide');
+                img_slide[0].classList.add('hide'); 
             }
             i++;
             if (i ==3) {
